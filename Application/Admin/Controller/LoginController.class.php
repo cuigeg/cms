869a4e1dbed8b2dc7f72c49dae21f1e37d8f96fc
cuigeg <18238205232@163.com>
2017-05-03 01:27:45 +0800

    cms

---
+++ Application/Admin/Controller/LoginController.class.php
@@ -0,0 +1,55 @@
+<?php
+namespace Admin\Controller;
+use Think\Controller;
+
+/**
+ * use Common\Model 这块可以不需要使用，框架默认会加载里面的内容
+ */
+class LoginController extends Controller
+{
+    public function index()
+    {
+        if(session('adminUser'))
+        {
+           $this->redirect('/admin.php');
+        }
+        // admin.php?c=index
+        $this->display();
+    }
+
+    public function check()
+    {
+        $username=$_POST['username'];
+        $password=$_POST['password'];
+
+        if(!trim($username))
+        {
+           return show (0,"用户名不能为空");
+        }
+        if(!trim($password))
+        {
+            return show (0,"密码不能为空");
+        }
+
+        $res=D('Admin')->getAdminByUsername($username);
+        if(!$res)
+        {
+            return show(0,"该用户不存在");
+        }
+
+        if($res['password'] != getMd5Password($password) )
+        {
+            return show(0,"密码错误");
+        }
+
+        session('adminUser',$res);
+        return show(1,"登录成功");
+    }
+
+    public function loginout()
+    {
+        session('adminUser', null);
+        $this->redirect('/admin.php?c=login');
+    }
+
+}
---
+++ Application/Common/Conf/config.php
@@ -0,0 +1,12 @@
+<?php
+return array(
+	//'配置项'=>'配置值'
+
+    //URL地址不区分大小写
+    'URL_CASE_INSENSITIVE' =>true,
+    'URL_MODEL'=>0,
+    'LOAD_EXT_CONFIG' => 'db',
+//    'LOAD_EXT_CONFIG' => 'db,XX,XX'       加载多个配置
+    'MD5_PRE' => 'sing_cms',
+    'HTML_FILE_SUFFIX' => '.html',
+);
